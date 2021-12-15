5f87d71114d004b28e80ad278b4c87ae46604a1b
MapWin <marvinfazbers@gmail.com>
2021-12-15 08:04:54 +0000

    Added new flags (Nano & Magisk 23.0)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,16 +19,24 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export FOX_RECOVERY_INSTALL_PARTITION=/dev/block/platform/bootdevice/by-name/recovery
         export FOX_VENDOR_INSTALL_PARTITION=/dev/block/platform/bootdevice/by-name/vendor
         export FOX_SYSTEM_INSTALL_PARTITION=/dev/block/platform/bootdevice/by-name/system
-        # OrangeFox
-	export OF_SCREEN_H=2160
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_USE_MAGISKBOOT=1
-	export OF_AB_DEVICE=0
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export FOX_DISABLE_APP_MANAGER=1
-	export FOX_DELETE_AROMAFM=1
-	export OF_MAINTAINER=MapWin
-	add_lunch_combo omni_"$FDEVICE"-eng
-	add_lunch_combo omni_"$FDEVICE"-userdebug
+# OrangeFox
+        export OF_SCREEN_H=2160
+        export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+        export OF_USE_MAGISKBOOT=1
+        export OF_AB_DEVICE=0
+        export FOX_USE_NANO_EDITOR=1
+        export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+        export FOX_DISABLE_APP_MANAGER=1
+        export FOX_DELETE_AROMAFM=1
+        export FOX_USE_SPECIFIC_MAGISK_ZIP="~/certus_additions/Magisk-23.0.zip"
+        export TARGET_DEVICE_ALT="certus"
+        export OF_TARGET_DEVICES="cactus,certus,certus64"
+        export OF_MAINTAINER=MapWin
+        export OF_MAINTAINER_AVATAR="~/certus_additions/avatar.png"
+        export FOX_VERSION="R11.1_3"
+        export FOX_VARIANT=default
+        export FOX_BUILD_TYPE=Stable
+        add_lunch_combo omni_"$FDEVICE"-eng
+        add_lunch_combo omni_"$FDEVICE"-userdebug
 fi
 #
