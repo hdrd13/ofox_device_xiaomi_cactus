ffd48a12e2325b325a3d25e10b4e1b1d7f4ea223
hdrd13 <96956239+hdrd13@users.noreply.github.com>
2024-02-11 17:41:31 +0200

    initial commit by me xd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,6 +21,8 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export FOX_SYSTEM_INSTALL_PARTITION=/dev/block/platform/bootdevice/by-name/system
 # OrangeFox
         export OF_SCREEN_H=2160
+        export OF_STATUS_INDENT_LEFT=32
+	     export OF_STATUS_INDENT_RIGHT=32
         export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
         export OF_USE_MAGISKBOOT=1
         export OF_AB_DEVICE=0
@@ -28,11 +30,11 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
         export FOX_DISABLE_APP_MANAGER=1
         export FOX_DELETE_AROMAFM=1
-        export FOX_USE_SPECIFIC_MAGISK_ZIP="~/certus_additions/Magisk-23.0.zip"
+        export FOX_USE_SPECIFIC_MAGISK_ZIP="~/Magisk-27.0.zip"
         export TARGET_DEVICE_ALT="certus"
         export OF_TARGET_DEVICES="cactus,certus,certus64"
-        export OF_MAINTAINER=MapWin
-        export OF_MAINTAINER_AVATAR="~/certus_additions/avatar.png"
+        export OF_MAINTAINER=hdrd13
+        export OF_MAINTAINER_AVATAR="~/avatar.png"
         export FOX_VERSION="R11.1_3"
         export FOX_VARIANT=default
         export FOX_BUILD_TYPE=Stable
